b6c6e8c8edf6ba6fb61a990f981238cc8e07d6cb
Danya <kurdukovdanila234@gmail.com>
2023-01-05 15:27:55 +0300

    Tie graph, FragmentContainerView and Toolbar together

--- app/src/main/java/com/example/vendingapp20/MainActivity.java
+++ app/src/main/java/com/example/vendingapp20/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.vendingapp20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavHostFragment navHostFragment =(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
+                .build();
+        Toolbar mainAppBar = findViewById(R.id.app_bar);
+        setSupportActionBar(mainAppBar);
+        getSupportActionBar().setTitle("Выберите действие");
+        NavigationUI.setupWithNavController(mainAppBar,navController,appBarConfiguration);
     }
 }
