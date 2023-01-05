a2ef471bbe393973c1e187f5be06c93d4ffbc34a
Danya <kurdukovdanila234@gmail.com>
2023-01-05 15:43:23 +0300

    Added UserFragment and style for TextView

---
+++ app/src/main/java/com/example/vendingapp20/fragments/UserFragment.java
@@ -0,0 +1,56 @@
+package com.example.vendingapp20.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vendingapp20.R;
+
+
+public class UserFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public UserFragment() {
+
+    }
+
+
+    public static UserFragment newInstance(String param1, String param2) {
+        UserFragment fragment = new UserFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_user, container, false);
+
+
+
+        return rootView;
+    }
+}
